53aefb1db3e7f6e58e96915402e54af56a8cd010
MuthuMaha <muthumaharajan1992@gmail.com>
2019-09-17 05:58:58 +0530

    first

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+    public function import() 
+    {
+        Excel::import(new UsersImport, 'users.csv');
+        
+        return redirect('/')->with('success', 'All good!');
+    }
+}
